7198cedd19e51d7f07be63553f8844fdf24478be
Rasiol Isufaj <rasiolisufaj47@gmail.com>
2022-09-12 15:51:58 +0200

    local storage

--- Chap 12 Async JavaScript/async.js
+++ Chap 12 Async JavaScript/async.js
@@ -1,17 +1,19 @@
 // Async & Await
 
 const getTodos = async () => {
-  const resource = await fetch("todoss/luigi.json");
+  //Thread 1
+  const resource = await fetch("todoss/luigi.json"); //Thread 2 + block Thread 1
 
   if (resource.status !== 200) {
-    throw new Error("Can not fetch the data");
+    //Thread 1
+    throw new Error("Can not fetch the data"); //Thread 1
   }
 
-  const data = await resource.json();
+  const data = await resource.json(); //Thread 2 + block thread 1
 
-  return data;
+  return data; //Thread 1
 };
 
-getTodos()
+getTodos() //Thread 0
   .then((data) => console.log("Resolved:", data))
   .catch((error) => console.log("Rejected:", error.message));
